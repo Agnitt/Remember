711c34b2669a89ef353c627d9510327db797beca
Agnitt <agnitt@yandex.ru>
2024-01-22 20:34:34 +0300

    Change message

--- app/src/main/java/com/agnitt/remember/MainActivity.kt
+++ app/src/main/java/com/agnitt/remember/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.agnitt.remember.ui.theme.RememberTheme
 
@@ -36,7 +37,7 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier.weight(7f),
                             contentAlignment = Alignment.Center
                         ) {
-                            Greeting("Android")
+                            Greeting("I hate it cuz feel self so stupid and embarrassed")
                         }
                         Switch(
                             modifier = Modifier
@@ -53,10 +54,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(msg: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hi, dorks! $msg T_T",
+        modifier = modifier,
+        textAlign = TextAlign.Center
     )
 }
 
